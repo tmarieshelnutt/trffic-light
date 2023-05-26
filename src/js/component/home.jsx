9518873abb81ee7b0a8ba8353ae67b28b511bd1d
Tara <tmarieshelnutt@gmail.com>
2023-05-26 11:54:03 +0000

    again

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,24 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+// export function Home() {
+//     const [selectedColor, setselectedColor] = useState("red");
+
+
+//     return (
+    
+//         <div className="traffic-light">
+//             <div className></div>
+//             <div onClick={() => setselectedColor ("red")}
+//             className={"light red" + ((selectedColor === "red") ? " glow" : "")}></div>
+//             <div onClick={() => setselectedColor ("yellow")}
+//             className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}></div>
+//             <div onClick={() => setselectedColor ("green")}
+//             className={"light green" + ((selectedColor === "green") ? " glow" : "")}></div>
+//             {/* <div className="light glow" onClick=></div> */}
+//         </div>
+//     )
+// }
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,34 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import Home from "./component/home.jsx";
+
+export function Home() {
+    const [selectedColor, setselectedColor] = useState("red");
+
+
+    return (
+        
+
+        <div className="traffic-light">
+            <div className></div>
+            <div onClick={() => setselectedColor ("red")}
+            className={"light red" + ((selectedColor === "red") ? " glow" : "")}></div>
+            <div onClick={() => setselectedColor ("yellow")}
+            className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}></div>
+            <div onClick={() => setselectedColor ("green")}
+            className={"light green" + ((selectedColor === "green") ? " glow" : "")}></div>
+            {/* <div className="light glow" onClick=></div> */}
+        </div>
+    );
+}
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
